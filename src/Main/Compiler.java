12a2b6b03dedafb64d8ee91474cf92b82e5bb75c
= <acdpf000@gmail.com>
2021-02-13 13:25:13 -0600

    Txt class added to drive files in proyect

--- src/Main/Compiler.java
+++ src/Main/Compiler.java
@@ -5,6 +5,8 @@
  */
 package Main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -165,6 +167,8 @@ public class Compiler extends javax.swing.JFrame {
     private void exampleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exampleActionPerformed
         input.setText("\n\n\n\nint x;\ndouble y;\nint m=3;\n");
         output.setText("");
+        String[] l = {"Hola mundo jaja", "Por que siempre decimos hola mundo gg"};
+        ArrayList<String> lines = (ArrayList) Arrays.asList(l);
     }//GEN-LAST:event_exampleActionPerformed
 
     private void cleanOutput() {
---
+++ src/Main/Txt.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author Angel
+ */
+public class Txt {
+
+    ArrayList<String> lines = new ArrayList();
+    File file;
+
+    public Txt(String nameFile) {
+        this.file = new File(nameFile + ".txt");
+    }
+
+    public ArrayList<String> getLines() {
+        lines.clear();
+        try {
+            FileReader fr = new FileReader(this.file);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+
+        return this.lines;
+    }
+
+    public void addLine(String line) {
+        FileWriter fw = null;
+        PrintWriter pw = null;
+        try {
+            fw = new FileWriter(this.file, true);
+            pw = new PrintWriter(fw);
+            pw.append(line + "\n");
+        } catch (IOException e) {
+            System.err.println(e);
+        } finally {
+            try {
+                if (null != fw) {
+                    fw.close();
+                }
+            } catch (IOException e2) {
+                System.err.println(e2);
+            }
+        }
+    }
+
+    public void addContent(ArrayList<String> lines) {
+        FileWriter fw = null;
+        PrintWriter pw = null;
+        try {
+            fw = new FileWriter(this.file, true);
+            pw = new PrintWriter(fw);
+            for (String l : lines) {
+                pw.println(l);
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        } finally {
+            try {
+                if (null != fw) {
+                    fw.close();
+                }
+            } catch (IOException e2) {
+                System.err.println(e2);
+            }
+        }
+    }
+}
